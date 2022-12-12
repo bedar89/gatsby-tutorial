75917fbb39c4da44069840acbb279a042dfaf143
bedar89 <2621954+bedar89@users.noreply.github.com>
2022-12-12 15:55:43 +0100

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,5 +8,30 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 module.exports = {
-  plugins: [],
+  plugins: [
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, // Needed for dynamic images
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images/`,
+      },
+    },
+  ],
+  siteMetadata: {
+    title: 'Web Warrior',
+    description: 'Web dev portfolio',
+    copyright: 'This website is copyright 2022 Web Warrior',
+    contact: 'me@thewebwarrior.be'
+  }
 }
---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+export default function About() {
+  return (
+    <Layout>
+    <div>
+        <h1>About Page</h1>
+        <p>Sunt labore ad culpa ipsum in cupidatat reprehenderit aliquip consectetur mollit quis deserunt.</p>
+        <p>Sunt labore ad culpa ipsum in cupidatat reprehenderit aliquip consectetur mollit quis deserunt.</p>
+        <p>Sunt labore ad culpa ipsum in cupidatat reprehenderit aliquip consectetur mollit quis deserunt.</p>
+    </div>
+    </Layout>
+  )
+}
